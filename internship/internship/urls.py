4a17f697b11ac7cc738c4802a28d65ce39833b3c
Harshani <ha1088@wildcats.unh.edu>
2020-11-03 11:17:13 -0500

    updated urls.py

--- internship/internship/urls.py
+++ internship/internship/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from internship_app.views import HomepageView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', HomepageView.as_view(), name='home'),
 ]
