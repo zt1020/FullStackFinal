39c0ab9f22864900e79b87e0e3c377fb04fc67ef
snehitha <sm1552@wildcats.unh.edu>
2020-11-07 16:45:33 -0500

    Fixed pylint errors

--- internship/apps.py
+++ internship/apps.py
@@ -1,5 +1,16 @@
+"""
+apps.py
+Contributors: Harshani, Snehitha
+Created : 11/3/2020
+Updated : 11/7/2020
+"""
+
+
 from django.apps import AppConfig
 
 
 class InternshipConfig(AppConfig):
+    """
+    Created a new app internship
+    """
     name = 'internship'
--- internship/views.py
+++ internship/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render
+"""
+views.py
+Contributors: Harshani, Snehitha
+Created : 11/3/2020
+Updated : 11/7/2020
+"""
 from django.views.generic import TemplateView
 
 class HomepageView(TemplateView):
+    """
+    This creates class based HomepageView
+    """
     template_name = 'index.html'
