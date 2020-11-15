f75e100e1af4d7d103c1813874c47a992f4d8661
snehitha <sm1552@wildcats.unh.edu>
2020-11-15 09:56:06 -0500

    Restructured models.py

--- internship/admin.py
+++ internship/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 #from internship.models.Student import Student
-from internship.models.Student import Student,Internship,InternshipAssignment
+from internship.models import Student,Internship,InternshipAssignment
 admin.site.register(Student)
 admin.site.register(Internship)
 admin.site.register(InternshipAssignment)
