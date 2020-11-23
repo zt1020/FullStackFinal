a735bcc1a87b77f4eaf5d7c4b0a5e14b8484c45e
snehitha <sm1552@wildcats.unh.edu>
2020-11-23 04:39:36 -0500

    Added forms for fields in INternshipAssignment

--- internship/forms.py
+++ internship/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Student,Internship
+from .models import Student,Internship,InternshipAssignment
 
 
 class StudentSearchForm(forms.ModelForm):
@@ -12,3 +12,9 @@ class InternshipSearchForm(forms.ModelForm):
    class Meta:
      model = Internship
      fields = ['organization_name', 'supervisor_name']
+
+
+class InternshipAssignmentSearchForm(forms.ModelForm):
+   class Meta:
+     model = InternshipAssignment
+     fields = ['semester', 'year']
