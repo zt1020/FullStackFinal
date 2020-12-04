cef0f91bd1d40300439ddb33495ed21437c17793
Harshani <ha1088@wildcats.unh.edu>
2020-12-04 01:14:05 -0500

    added url to redirect to update_student page

--- internship/urls.py
+++ internship/urls.py
@@ -5,7 +5,7 @@ updated : 11/16/2020
 """
 from django.urls import path
 # from django.conf.urls import url
-from internship.views import HomepageView,display_students,display_internship,register_page
+from internship.views import HomepageView,display_students,display_internship,register_page,updateStudent
 from internship.views import display_internshipassignment
 from internship import views
 
@@ -21,5 +21,7 @@ urlpatterns = [
             name='display_internshipassignment'),
         path('register/', register_page, name='register'),
         path('logout/', views.logout_request, name='logout'),
-        path('login/', views.login_request, name='login')
+        path('login/', views.login_request, name='login'),
+        path('update_student/<int:pk>/', views.updateStudent, name="update_student")
+        # url(r'^user/create$', UserCreateView.as_view(), name='user_create'),
     ]
