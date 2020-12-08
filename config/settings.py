d7ace11933ebedeb893789d4b2b2e259b7d1ef03
Harshani <ha1088@wildcats.unh.edu>
2020-12-08 17:20:45 -0500

    deploying to heroku

--- config/settings.py
+++ config/settings.py
@@ -10,8 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 from pathlib import Path
-# import django_heroku
-# import os
+import django_heroku
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -135,4 +135,4 @@ STATIC_URL = '/static/'
 
 
 # Activate Django-Heroku.
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
